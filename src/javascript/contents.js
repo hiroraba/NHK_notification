54a96be4d0c9e14c02f6208c8589ba0737d73f5d
hiroraba <hiroraba@gmail.com>
2014-01-28 01:09:00 +0900

    Fix notice mode

--- src/javascript/contents.js
+++ src/javascript/contents.js
@@ -1,33 +1,98 @@
 ({
+
+  base_url_now : 'http://api.nhk.or.jp/v1/pg/now',
+  base_url_list : 'http://api.nhk.or.jp/v1/pg/list',
+  area : '130', //Tokyo
+  service : 'g1',
+  program: [],
+
   init : function(){
     var self = this;
     $(function() {
-      self.bindClick();
-      self.bindWait();
+      self.getNowOnair();
+      self.getNHKprogram();
+      self.pollingProgram();
     });
   },
 
-  bindClick: function() {
+  pollingProgram: function() {
     var self = this;
+    setInterval(function(){ self.noticeProgram(); }, 1000 * 60);
+  },
+
+  getNowOnair: function() {
+    var self = this;
+    var url_now = this.base_url_now + "/" + this.area + "/" + this.service + ".json";
     $("#nhk").click(function(){
-      self.callApi();
+      self.callApi(url_now);
     });
   },
 
-  bindWait: function() {
+  getNHKprogram: function() {
     var self = this;
-    setInterval(function(){ self.callApi() }, 1000 * 60 * 60);
+    var today = self.getToday();
+    var list_url = this.base_url_list + "/" + this.area + "/" + this.service + "/" + today + ".json";
+    var program = self.callApi(list_url);
   },
 
-  callApi: function() {
+  saveNHKprogram: function(program) {
+    var program_array = program.list.g1;
+    for (var i = 0 ; i < program_array.length; i++) {
+      this.program[i] = {};
+      this.program[i]["title"] = program_array[i].title;
+      this.program[i]["subtitle"] = program_array[i].subtitle;
+      this.program[i]["icon"] = program_array[i].service.logo_m.url;
+      this.program[i]["start_time"] = Date.parse(program_array[i].start_time);
+    }
+  },
+
+  noticeProgram: function() {
     var self = this;
-    var url = "http://api.nhk.or.jp/v1/pg/now/130/g1.json";
+    var now = new Date();
+    for (var i = 0 ; i < this.program.length; i++) {
+      var diff = Math.abs(now - this.program[i]["start_time"]);
+      console.log(diff);
+      if (diff <= 1000 * 60) {
+      self.sendNotification(this.program[i]["icon"], this.program[i]["title"], this.program[i]["subtitle"]);
+      }
+    }
+  },
+
+  getToday: function() {
+    var self = this;
+    var date = new Date();
+    var hour = date.getHours();
+    if (hour < 4) {
+      date.setDate(date.getDate()-1); 
+    }
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHour;
+    month = ('0' + month).slice(-2);
+    day = ('0' + day).slice(-2);
+    return year + "-" + month + "-" + day;
+  },
+
+  callApi: function(api_url) {
+    var self = this;
     var api_key = localStorage["api_key"];
+    var url = api_url + "?key=" + api_key;
     $.ajax ({
       type: "GET",
-      url : url + "?key=" +api_key,
+      url : url.replace(/\s+/g, ""),
+      datatype: 'json',
       success: function(msg) {
-        self.sendNotification(msg);
+       for(var key in msg) {
+         if (key == "list") {
+           self.saveNHKprogram(msg);
+         } else if (key == "nowonair_list") {
+           var title = msg.nowonair_list.g1.present.title;
+           var icon = msg.nowonair_list.g1.present.service.logo_s.url;
+           var subtitle = msg.nowonair_list.g1.present.subtitle;
+           self.sendNotification(icon, title, subtitle);
+         }
+       }
       },
       failure: function() {
         console.log("Fairure");
@@ -35,13 +100,10 @@
     });
   },
 
-  sendNotification: function(msg){
-    var title = msg.nowonair_list.g1.present.title;
-    var icon = msg.nowonair_list.g1.present.service.logo_s.url;
-    var message = msg.nowonair_list.g1.present.subtitle;
-    window.webkitNotifications.createNotification(icon, title, message);
+  sendNotification: function(icon, title, subtitle) {
+    window.webkitNotifications.createNotification(icon, title, subtitle);
     if ( webkitNotifications.checkPermission() == 0 ) {
-      var popup = webkitNotifications.createNotification(icon, title, message);
+      var popup = webkitNotifications.createNotification(icon, title, subtitle);
       popup.ondisplay = function(){
         setTimeout(function(){
           popup.cancel();
