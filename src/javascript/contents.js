fc9345565c85107f5c7bff3ae94044b7373280a3
hiroraba <hiroraba@gmail.com>
2014-01-28 22:59:29 +0900

    add choice service

--- src/javascript/contents.js
+++ src/javascript/contents.js
@@ -3,24 +3,22 @@
   base_url_now : 'http://api.nhk.or.jp/v1/pg/now',
   base_url_list : 'http://api.nhk.or.jp/v1/pg/list',
   area : '130', //Tokyo
-  service : 'g1',
+  service: "g1",
   program: [],
 
   init : function(){
     var self = this;
+    var service = localStorage["service"].split(",");
     $(function() {
       self.getNowOnair();
-      self.getNHKprogram();
       self.pollingProgram();
+      for (var i = 0; i < service.length; i++) {
+        self.getNHKprogram(service[i]);
+      }
     });
   },
 
-  pollingProgram: function() {
-    var self = this;
-    setInterval(function(){ self.noticeProgram(); }, 1000 * 30);
-  },
-
-  getNowOnair: function() {
+  getNowOnair: function(service) {
     var self = this;
     var url_now = this.base_url_now + "/" + this.area + "/" + this.service + ".json";
     $("#nhk").click(function(){
@@ -28,21 +26,25 @@
     });
   },
 
-  getNHKprogram: function() {
+  pollingProgram: function() {
+    var self = this;
+    setInterval(function(){ self.noticeProgram(); }, 1000 * 20);
+  },
+
+  getNHKprogram: function(service) {
     var self = this;
     var today = self.getToday();
-    var list_url = this.base_url_list + "/" + this.area + "/" + this.service + "/" + today + ".json";
-    var program = self.callApi(list_url);
+    var list_url = this.base_url_list + "/" + this.area + "/" + service + "/" + today + ".json";
+    self.callApi(list_url);
   },
 
   saveNHKprogram: function(program) {
-    var program_array = program.list.g1;
-    for (var i = 0 ; i < program_array.length; i++) {
+    for (var i = this.program.length ; i < program.length; i++) {
       this.program[i] = {};
-      this.program[i]["title"] = program_array[i].title;
-      this.program[i]["subtitle"] = program_array[i].subtitle;
-      this.program[i]["icon"] = program_array[i].service.logo_m.url;
-      this.program[i]["start_time"] = Date.parse(program_array[i].start_time);
+      this.program[i]["title"] = program[i].title;
+      this.program[i]["subtitle"] = program[i].subtitle;
+      this.program[i]["icon"] = program[i].service.logo_m.url;
+      this.program[i]["start_time"] = Date.parse(program[i].start_time);
       this.program[i]["notice"] = 0;
     }
   },
@@ -52,7 +54,7 @@
     var now = new Date();
     for (var i = 0 ; i < this.program.length; i++) {
       var diff = Math.abs(now - this.program[i]["start_time"]);
-      if (diff < 1000 * 60 && this.program[i]["notice"] == 0) {
+      if (diff < 1000 * 20 && this.program[i]["notice"] == 0) {
         this.program[i]["notice"] = 1;
         self.sendNotification(this.program[i]["icon"], this.program[i]["title"], this.program[i]["subtitle"]);
       }
@@ -79,21 +81,22 @@
     var self = this;
     var api_key = localStorage["api_key"];
     var url = api_url + "?key=" + api_key;
+    console.log(url);
     $.ajax ({
       type: "GET",
       url : url.replace(/\s+/g, ""),
       datatype: 'json',
       success: function(msg) {
-       for(var key in msg) {
-         if (key == "list") {
-           self.saveNHKprogram(msg);
-         } else if (key == "nowonair_list") {
-           var title = msg.nowonair_list.g1.present.title;
-           var icon = msg.nowonair_list.g1.present.service.logo_m.url;
-           var subtitle = msg.nowonair_list.g1.present.subtitle;
-           self.sendNotification(icon, title, subtitle);
-         }
-       }
+        for(var key in msg) {
+          if (key == "list") {
+            self.dispatchList(msg.list, "list");
+          } else if (key == "nowonair_list") {
+            var icon = msg.nowonair_list.g1.present.service.logo_m.url;
+            var title = msg.nowonair_list.g1.present.title;
+            var subtitle = msg.nowonair_list.g1.present.subtitle;
+            self.sendNotification(icon, title, subtitle);
+          }
+        }
       },
       failure: function() {
         console.log("Fairure");
@@ -101,7 +104,33 @@
     });
   },
 
-  sendNotification: function(icon, title, subtitle) {
+  dispatchList: function(list) {
+    var self = this;
+    for (var key in list) {
+      switch(key) {
+        case "g1":
+          service = list.g1;
+          break;
+        case "e1":
+          service = list.e1;
+          break;
+        case "s1":
+          service = list.s1;
+          break;
+        case "s3":
+          service = list.s3;
+          break;
+        case "r3":
+          servive = list.r3;
+          break;
+        default:
+          break;
+      }
+    }
+    self.saveNHKprogram(service);
+  },
+
+  sendNotification: function(icon,title,subtitle) {
     window.webkitNotifications.createNotification(icon, title, subtitle);
     if ( webkitNotifications.checkPermission() == 0 ) {
       var popup = webkitNotifications.createNotification(icon, title, subtitle);
